0ff6cb081ad700bca4673ad103203ad099d174e3
Bob4 <61025876+AngelBob4@users.noreply.github.com>
2024-09-17 23:58:48 +0300

    Revert "Update car calculator and main"

--- src/Main.java
+++ src/Main.java
@@ -11,18 +11,13 @@ public class Main {
         System.out.println("Введите цвет (WHITE, BLUE, YELLOW, RED, PEARLESCENT, METALLIC_GRAY):");
         String colorInput = scanner.nextLine().toUpperCase();
 
-        // Ввод данных для класса авто
-        System.out.println("Введите класс машины (ECONOMY COMFORT COMFORT_PLUS BUSINESS ULTIMATE");
-        String classInput = scanner.nextLine().toUpperCase();
-
         try {
             // Преобразуем ввод в enum
             CarCalculator.Part part = CarCalculator.Part.valueOf(partInput);
             CarCalculator.Color color = CarCalculator.Color.valueOf(colorInput);
-            CarCalculator.Class carClass = CarCalculator.Class.valueOf(classInput);
 
             // Рассчитываем стоимость покраски
-            double cost = CarCalculator.calculatePaintingCost(part, color, carClass);
+            double cost = CarCalculator.calculatePaintingCost(part, color);
 
             // Выводим результат
             System.out.printf("Стоимость покраски детали %s в цвет %s составляет %.2f рублей%n", partInput, colorInput, cost);
